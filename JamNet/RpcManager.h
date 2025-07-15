c3f65bab6c96cfce2b9eb5cf5a7348771628f36d
akxotjr <mataeseok@gmail.com>
2025-07-15 16:25:10 +0900

    07/15 16:25

---
+++ JamNet/RpcManager.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace jam::net
+{
+	class RpcManager
+	{
+		DECLARE_SINGLETON(RpcManager)
+	public:
+		template<typename T>
+		void Register(std::function<void(Sptr<Session>, const T&)> handler);
+
+		template<typename T>
+		void Call(Sptr<Session> session, const T& message, bool reliable = true);
+
+		void Dispatch(Sptr<Session> session, uint16 rpcId, const char* data, size_t len);
+
+
+	private:
+		xumap<uint16, std::function<void(Sptr<Session>, const char*, size_t)>> m_handlers;
+	};
+
+
+
+
+
+
+	template <typename T>
+	void RpcManager::Register(std::function<void(Sptr<Session>, const T&)> handler)
+	{
+
+	}
+
+	template <typename T>
+	void RpcManager::Call(Sptr<Session> session, const T& message, bool reliable)
+	{
+	}
+
+	inline void RpcManager::Dispatch(Sptr<Session> session, uint16 rpcId, const char* data, size_t len)
+	{
+	}
+}
+
